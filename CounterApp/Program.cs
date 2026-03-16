3a266f23393e872b05152f13785bcc0b305497f5
MateuszJastrzembski <92751694+mateuszjastrzembski3141592@users.noreply.github.com>
2026-03-16 17:47:44 +0100

    feat(CounterApp): implement event publisher and subscriber logic
    - Created Counter class with ThresholdReached event.
    
    - Implemented custom ThresholdReachedEventArgs payload.
    
    - Added interactive loop to trigger and handle events.
    
    - Updated README.md.

---
+++ CounterApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CounterApp;
+
+class Program
+{
+    static void Main()
+    {
+        Counter counter = new(10);
+
+        counter.ThresholdReached += Counter_ThresholdReached;
+
+        Console.WriteLine("Press \"a\" to add 1 to the counter or \"q\" to quit");
+
+        while (true)
+        {
+            var key = Console.ReadKey(true).KeyChar;
+
+            if (key == 'a')
+            {
+                counter.Add(1);
+            }
+            else if (key == 'q')
+            {
+                break;
+            }
+        }
+    }
+
+    static void Counter_ThresholdReached(object? sender, ThresholdReachedEventArgs e)
+    {
+        Console.WriteLine($"Threshold of {e.Threshold} reached at {e.TimeReached}.");
+    }
+}
